198bd54eaeb65c2aa61beba41f2cf01a216ce2f4
Jose-Araujo <jose.vinicius450@gmail.com>
2021-08-12 15:07:44 -0300

    Deploy do blog

--- java/blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ java/blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -21,6 +21,16 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService);
 	}
+	/*
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+		auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(passwordEncoder().encode("root"))
+		.authorities("ROLE_USER");
+	}*/
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -29,7 +39,15 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests().antMatchers("/usuarios/logar").permitAll().antMatchers("/usuarios/cadastrar").permitAll().anyRequest().authenticated().and().httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/usuarios/logar").permitAll()
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().cors()
+		.and().csrf().disable();
 	}
 	
 }
